5b7003e25f21657b6bbe8fab65b4977673923383
sumathi3399 <sumathivarma3399@gmail.com>
2022-02-01 18:51:31 +0530

    first java practice

---
+++ src/com/sumathi/Main.java
@@ -0,0 +1,16 @@
+package com.sumathi;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hi Sumathi");
+        System.out.println("please enter your marks");
+        Scanner marks = new Scanner(System.in);
+        int s_marks = marks.nextInt();
+        System.out.println("your marks is " + s_marks);
+
+    }
+}
+
